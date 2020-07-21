0a1141e6b3341e4164b8e6ba761e636dfa55f6e0
taejinseok <stj930@gmail.com>
2020-07-21 16:08:34 +0900

    refactor: edit coffin

--- src/main/java/com/jenkin/prac/PracApplication.java
+++ src/main/java/com/jenkin/prac/PracApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracApplication {
 
     public static void main(String[] args) {
-        System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿⚰️😇🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
+        System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿😇🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
+        System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿⚰️🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
+        System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
         SpringApplication.run(PracApplication.class, args);
     }
 
