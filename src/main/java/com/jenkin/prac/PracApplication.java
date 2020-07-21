e32ab734bb850d62d4d904a256170a18a8e9b892
taejinseok <stj930@gmail.com>
2020-07-21 15:27:23 +0900

    feat: coffin dance

--- src/main/java/com/jenkin/prac/PracApplication.java
+++ src/main/java/com/jenkin/prac/PracApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracApplication {
 
     public static void main(String[] args) {
+        System.out.println("   🕺🏿");
+        System.out.println(" 🕺🏿⚰️🕺🏿");
+        System.out.println(" 🕺🏿  🕺🏿");
         SpringApplication.run(PracApplication.class, args);
     }
 
