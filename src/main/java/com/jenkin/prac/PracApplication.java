ff8015c2c4110b373edb58c37da2738a9da11f27
taejinseok <stj930@gmail.com>
2020-07-23 15:58:07 +0900

    refactor: too many coffin

--- src/main/java/com/jenkin/prac/PracApplication.java
+++ src/main/java/com/jenkin/prac/PracApplication.java
@@ -7,12 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PracApplication {
 
     public static void main(String[] args) {
-        System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿⚰️🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
         System.out.println("🕺🏿🕺🏿⚰️⚰️⚰️⚰️⚰️⚰️⚰️⚰️🕺🏿");
         System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿⚰️🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
         System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿⚰️🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
         System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿⚰️🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
-        System.out.println("🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿⚰️🕺🏿🕺🏿🕺🏿🕺🏿🕺🏿");
+        System.out.println("🕺🏿🕺🏿⚰️⚰️⚰️⚰️⚰️⚰️⚰️⚰️🕺🏿");
         SpringApplication.run(PracApplication.class, args);
     }
 
